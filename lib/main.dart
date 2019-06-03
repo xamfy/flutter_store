56fe4ccb07484bd49ff32304d7fdace50086e0e3
xamfy <19357995+xamfy@users.noreply.github.com>
2019-06-04 02:37:20 +0530

    Add status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -10,6 +11,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.grey, //or set color with: Color(0xFF0000FF)
+    ));
+
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Store',
